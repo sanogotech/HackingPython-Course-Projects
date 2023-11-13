fb2b3f9eb5bc8c5be9e57f87c8810648b4187c10
SOULEYSANOGO <SOULEYSANOGO@CIEW15854>
2023-11-13 18:01:05 +0000

    Update

--- scanner.py
+++ scanner.py
@@ -1,1 +1,33 @@
 import requests
+from  bs4 import BeautifulSoup
+
+bad_char = ["'","<",">","(",")",";"]
+
+
+def  spider_inputs(url):
+
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    form = soup.find('form')
+    
+    if not form:
+        print("No forms was found")
+        return []
+    
+    return form.find_all('input')
+
+def  test_inputs(url):
+
+def main():
+    url = input("Enter url to Scan")
+    
+    inputs = spider_inputs(url)
+    
+    if not  inputs 
+        return
+    
+    
+
+
+if __name__="__main__":
+    main()
