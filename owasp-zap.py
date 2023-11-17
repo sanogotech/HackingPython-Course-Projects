7f946fdf6353d2d49b84090ee507586b32aa9cec
SANOGOTECH <sanogotech@yahoo.fr>
2023-11-17 13:58:41 +0000

    Update owasp-zap.py

--- owasp-zap.py
+++ owasp-zap.py
@@ -1,1 +1,22 @@
 
+import zapapi
+
+def main():
+    # Demander à l'utilisateur l'adresse URL du site Web à scanner
+    url = input("Adresse URL du site Web à scanner : ")
+
+    # Se connecter à OWASP ZAP
+    zap = zapapi.Zap()
+    zap.open()
+
+    # Lancer un scan ZAP
+    scan = zap.spider.scan(url)
+    scan.wait()
+
+    # Analyser la réponse HTTP pour détecter les vulnérabilités
+    vulnerabilities = scan.get_results()
+    for vulnerability in vulnerabilities:
+        print("Le site Web {} est vulnérable à {}".format(url, vulnerability.name))
+
+if __name__ == "__main__":
+    main()
